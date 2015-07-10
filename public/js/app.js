6a73e7fa34a24023511700fafda32e6e06a5721d
Guillermo Gallo <Guillermo.Gallo@wnco.com>
2015-07-10 08:01:36 -0500

    Adding frontend app files

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+var expressTestApp = angular.module('expressTestApp', [
+  'ngRoute',
+  'expressTestAppControllers'
+]);
+
+expressTestApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/login', {
+        templateUrl: 'partials/login.html',
+        controller: 'LoginCtrl'
+      }).
+      when('/articles-list', {
+        templateUrl: 'partials/articles-list.html',
+        controller: 'ArticlesListCtrl'
+      }).
+      otherwise({
+        redirectTo: '/login'
+      });
+}]);
---
+++ public/js/controllers.js
@@ -0,0 +1,55 @@
+var expressTestAppControllers = angular.module('expressTestAppControllers', []);
+
+expressTestAppControllers.service('sharedProperties', function () {
+    
+    var property = null;
+
+    return {
+        getProperty: function () {
+            return property;
+        },
+        setProperty: function(value) {
+            property = value;
+        }
+    };
+});
+
+expressTestAppControllers.controller('LoginCtrl', function ($scope, $http, $location, sharedProperties) {
+    
+    $scope.authenticate = function(user, password) {
+	    $http.post('auth.json', {username: user, password: password})
+		    .success(function(data, status, headers, config) {		
+	  			sharedProperties.setProperty(data.token);
+	  			$location.path('/articles-list');
+		  	})
+		    .error(function(data, status, headers, config) {
+		  		alert("There has been an error processing your login request. Please contact system admin.");
+		  	});
+    };
+});
+
+expressTestAppControllers.controller('ArticlesListCtrl', function ($scope, $http, $location, sharedProperties) {
+  			
+	var token = sharedProperties.getProperty();
+	var url = 'articles.json';
+
+	$http({method: 'GET', url, headers: {token: token}})
+		.success(function(data) {
+			makeData(data);
+		})
+		.error(function(data) {
+		  	alert("There has been an error processing your articles list. Please contact system admin.");
+		});
+
+	function makeData (data) {
+		$scope.articles = [];
+		for (i = 0; i < data.length; i++) { 
+			$scope.articles[i] = data[i];
+		}
+		$scope.orderProp = 'author';
+	}
+
+	$scope.getBack = function () {
+		$location.path('/login');
+	}
+});
