1d051fcb78b005110f001f8725d25a83b1775ce7
Guillermo Gallo <Guillermo.Gallo@wnco.com>
2015-07-15 10:18:00 -0500

    minor remediations

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -17,16 +17,22 @@ expressTestAppControllers.service('sharedProperties', function () {
 expressTestAppControllers.controller('LoginCtrl', function ($scope, $http, $location, sharedProperties) {
     
     $scope.authenticate = function(user, password) {
-	    $http.post('auth.json', {username: user, password: password})
-		    .success(function(data) {
-		    	if (data.success == false) {
-	    			$location.path('/');
-	    			alert("There has been an error processing your login request. Please contact system admin.");
-		    	} else if (data.success == true) {
-		    		sharedProperties.setProperty(data.token);
-	  				$location.path('/articles-list');
-		    	};
-	 	  	});
+    	if (!user || !password) {
+    		alert("Please fill out both fields");
+    	} else {
+		    $http.post('auth.json', {username: user, password: password})
+			    .success(function(data) {
+			    	if (data.success == false) {
+		    			$location.path('/');
+		    			alert("Username or Password are incorrect. Please contact system admin.");
+			    	} else if (data.success == true) {
+			    		sharedProperties.setProperty(data.token);
+		  				$location.path('/articles-list');
+			    	};
+		 	  	});
+		}
+
+		$scope.user = user;
     };
 });
 
@@ -65,7 +71,7 @@ expressTestAppControllers.controller('ArticlesListCtrl', function ($scope, $http
 		};
 		
 		$scope.orderProp = 'author';
-	}
+	} 
 
 	$scope.getBack = function () {
 		$location.path('/login');
